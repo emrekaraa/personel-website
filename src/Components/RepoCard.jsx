a97e7ee33d425bd5c23b3e92540dbf533ef2becf
Emre Kara <emrekara.dev@gmail.com>
2022-01-29 16:26:40 +0300

    Repo Card Component Typo fixed

--- src/Components/RepoCard.jsx
+++ src/Components/RepoCard.jsx
@@ -15,14 +15,14 @@ const RepoCard = ({ name, desc, url, stars, language, forks, topics }) => {
         <>
             <Fade left duration={1200}>
                 <a href={`${url}`} className='repo-card'>
-                    <h3 className={`text-lg font-bold flex items-center text-textColor4`}>{name} <span className='ml-2'><GoLinkExternal /></span></h3>
+                    <h3 className={`text-lg font-bold flex items-center text-textColor4`}>{name}<span className='ml-2'><GoLinkExternal /></span></h3>
                     <p className='text-sm mt-2'>{desc}</p>
                     <div className='mt-2 flex flex-wrap'>{topics.map((topic) => {
                         return <span key={topic} className='topic'>{topic}</span>
                     })}</div>
 
-                    <div div className='flex items-center mt-10'>
-                        <p className='flex items-center mr-2.5'><span className={`circle ${language == "JavaScript" ? "bg-yellow-400" : language === "CSS" || "SCSS" ? "bg-pink-500" : "bg-blue-500"}`}></span>{language}</p>
+                    <div className='flex items-center mt-10'>
+                        <p className='flex items-center mr-2.5'><span className={`circle ${language == "JavaScript" ? "bg-yellow-400" : language == "CSS" || "SCSS" ? "bg-pink-500" : "bg-blue-500"}`}></span>{language}</p>
                         <span className='flex items-center mr-2.5'><span className='mr-0.5'><AiOutlineStar /></span>{stars}</span>
                         <span className='flex items-center mr-2.5'><span className='mr-0.5'><BiGitRepoForked /></span>{forks}</span>
 
