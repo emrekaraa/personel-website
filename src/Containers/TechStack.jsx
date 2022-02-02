d34ef6ffb75b41c5ec961716586800d8a88860c4
Emre Kara <emrekara.dev@gmail.com>
2022-02-02 17:12:25 +0300

    Tech Stack icon updated

--- src/Containers/TechStack.jsx
+++ src/Containers/TechStack.jsx
@@ -1,38 +1,92 @@
 // Css
-import '../Styles/Containers/TechStack.css';
+import "../Styles/Containers/TechStack.css";
 // Animations
 import { JackInTheBox } from "react-awesome-reveal";
 
 // Icons
 import {
-    IoLogoJavascript,
-    IoLogoReact,
-    IoLogoHtml5,
-    IoLogoCss3,
-    IoLogoSass
-} from 'react-icons/io5';
-import { SiTailwindcss, SiBootstrap, SiJquery, SiRedux } from 'react-icons/si'
+  IoLogoJavascript,
+  IoLogoReact,
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoSass,
+} from "react-icons/io5";
+import {
+  SiTailwindcss,
+  SiBootstrap,
+  SiJquery,
+  SiRedux,
+  SiNextdotjs,
+} from "react-icons/si";
 
 const TechStack = () => {
-    return (
-        <div className='bg-secondary' id='tech-stack'>
-            <div className='container lg:px-48 py-14'>
-                <h2 className='text-4xl text-textColor2 font-bold text-center'>Tech Stack</h2>
-                <ul className='tech-items'>
-                    <JackInTheBox triggerOnce duration={1000}>
-                        <li className='tech-item'><span><IoLogoHtml5 className='text-red-500 mr-2' /></span>HTML5</li>
-                        <li className='tech-item'><span><IoLogoCss3 className='text-blue-400 mr-2' /></span>CSS3</li>
-                        <li className='tech-item'><span><IoLogoSass className='text-pink-400 mr-2' /></span>Sass</li>
-                        <li className='tech-item'><span><SiBootstrap className='text-purple-500 mr-2' /></span>Bootstrap</li>
-                        <li className='tech-item'><span><SiTailwindcss className='text-blue-400 mr-2' /></span>Tailwind</li>
-                        <li className='tech-item'><span><SiJquery className='text-orange-400 mr-2' /></span>jQuery</li>
-                        <li className='tech-item'><span><IoLogoJavascript className='text-yellow-300 mr-2' /></span>JavaScript</li>
-                        <li className='tech-item'><span><IoLogoReact className='text-blue-400 mr-2' /></span>ReactJs</li>
-                        <li className='tech-item'><span><SiRedux className='text-purple-500 mr-2' /></span>Redux</li>
-                    </JackInTheBox>
-                </ul>
-            </div>
-        </div>)
+  return (
+    <div className="bg-secondary" id="tech-stack">
+      <div className="container lg:px-48 py-14">
+        <h2 className="text-4xl text-textColor2 font-bold text-center">
+          Tech Stack
+        </h2>
+        <ul className="tech-items">
+          <JackInTheBox triggerOnce duration={1000}>
+            <li className="tech-item">
+              <span>
+                <IoLogoHtml5 className="text-red-500 mr-2" />
+              </span>
+              HTML5
+            </li>
+            <li className="tech-item">
+              <span>
+                <IoLogoCss3 className="text-blue-400 mr-2" />
+              </span>
+              CSS3
+            </li>
+            <li className="tech-item">
+              <span>
+                <IoLogoSass className="text-pink-400 mr-2" />
+              </span>
+              Sass
+            </li>
+            <li className="tech-item">
+              <span>
+                <SiBootstrap className="text-purple-500 mr-2" />
+              </span>
+              Bootstrap
+            </li>
+            <li className="tech-item">
+              <span>
+                <SiTailwindcss className="text-blue-400 mr-2" />
+              </span>
+              Tailwind
+            </li>
+            <li className="tech-item">
+              <span>
+                <SiJquery className="text-orange-400 mr-2" />
+              </span>
+              jQuery
+            </li>
+            <li className="tech-item">
+              <span>
+                <IoLogoJavascript className="text-yellow-300 mr-2" />
+              </span>
+              JavaScript
+            </li>
+            <li className="tech-item">
+              <span>
+                <IoLogoReact className="text-blue-400 mr-2" />
+              </span>
+              ReactJs
+            </li>
+            <li className="tech-item">
+              <span>
+                <SiNextdotjs className="text-white mr-2 text-xl" />
+              </span>
+              NextJs
+            </li>
+          </JackInTheBox>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default TechStack;
