7ea4d7522df9534d685baedb90acebfb936e1292
Emre Kara <emrekara.dev@gmail.com>
2023-05-24 13:54:19 +0300

    edit

--- src/Containers/Projects.jsx
+++ src/Containers/Projects.jsx
@@ -1,57 +1,65 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 //Icons
 import { BiLinkExternal } from "react-icons/bi";
 //Components
-import RepoCard from '../Components/RepoCard';
+import RepoCard from "../Components/RepoCard";
 //Css
-import '../Styles/Containers/Projects.css';
+import "../Styles/Containers/Projects.css";
 //Animation
 import { Slide } from "react-awesome-reveal";
 
 const Projects = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get("https://api.github.com/users/emrekaraa/repos");
+    setData(response.data);
+  };
 
-    const getData = async () => {
-        const response = await axios.get('https://api.github.com/users/emrekaraa/repos');
-        setData(response.data);
-    }
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-
-
-    useEffect(() => {
-        getData();
-    }, [])
-
-
-    return (
-        <div className='bg-primary' id='projects'>
-            <div className='projects-container'>
-                <div className='flex justify-between'>
-                    <h2 className='text-4xl text-textColor2 font-bold text-center mb-3'>Projects </h2>
-                    <a href="https://github.com/emrekaraa" target="_blank" rel="noreferrer" className='flex items-center text-textColor1'>See more <BiLinkExternal className='ml-2' /></a>
-                </div>
-                <Slide triggerOnce>
-                    <div className="repos">
-                        {console.log(data)}
-                        {data?.filter(
-                            x =>
-                                x.name === "github-finder-react" ||
-                                x.name === "e-commerce-nextjs-redux" ||
-                                x.name === "react-crud-app" ||
-                                x.name === "google-clone-react" ||
-                                x.name === "netflix-clone-react" ||
-                                x.name === "twitter-clone-react",
-                        ).map(repo => (
-                            <RepoCard key={repo.id} name={repo.name} desc={repo.description} url={repo.html_url} stars={repo.stargazers_count} language={repo.language} forks={repo.forks_count} topics={repo.topics} />
-                        ))}
-                    </div>
-                </Slide>
-            </div>
+  return (
+    <div className="bg-primary" id="projects">
+      <div className="projects-container">
+        <div className="flex justify-between">
+          <h2 className="text-4xl text-textColor2 font-bold text-center mb-3">
+            Projects{" "}
+          </h2>
+          <a
+            href="https://github.com/emrekaraa"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center text-textColor1"
+          >
+            See more <BiLinkExternal className="ml-2" />
+          </a>
         </div>
-    )
+        <Slide triggerOnce>
+          <div className="repos">
+            {data
+              ?.filter(
+                (x) => x.name === "github-finder-react" || x.name === "personel-website"
+              )
+              .map((repo) => (
+                <RepoCard
+                  key={repo.id}
+                  name={repo.name}
+                  desc={repo.description}
+                  url={repo.html_url}
+                  stars={repo.stargazers_count}
+                  language={repo.language}
+                  forks={repo.forks_count}
+                  topics={repo.topics}
+                />
+              ))}
+          </div>
+        </Slide>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
